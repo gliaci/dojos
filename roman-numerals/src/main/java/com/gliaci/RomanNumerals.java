5e5bd42bafbdd50090cf424cdb5f94aaeb00e018
Giuseppe Liaci <giuseppe.liaci@rumbo.com>
2017-12-10 22:26:38 +0100

    refactored RomanNumerals

--- roman-numerals/src/main/java/com/gliaci/RomanNumerals.java
+++ roman-numerals/src/main/java/com/gliaci/RomanNumerals.java
@@ -5,34 +5,32 @@ import java.util.Map;
 
 public class RomanNumerals
 {
-  private Map<Integer, String> romanSymbolMap;
-  private StringBuilder romanNumeralSb;
+  private Map<Integer, String> romanSymbolsMap;
 
   public RomanNumerals()
   {
-    romanNumeralSb = new StringBuilder();
-
-    romanSymbolMap = new HashMap<>();
-    romanSymbolMap.put(1, "I");
-    romanSymbolMap.put(2, "II");
-    romanSymbolMap.put(3, "III");
-    romanSymbolMap.put(4, "IV");
-    romanSymbolMap.put(5, "V");
-    romanSymbolMap.put(9, "IX");
-    romanSymbolMap.put(10, "X");
-    romanSymbolMap.put(40, "XL");
-    romanSymbolMap.put(50, "L");
-    romanSymbolMap.put(90, "XC");
-    romanSymbolMap.put(100, "C");
-    romanSymbolMap.put(400, "CD");
-    romanSymbolMap.put(500, "D");
-    romanSymbolMap.put(900, "CM");
-    romanSymbolMap.put(1000, "M");
+    romanSymbolsMap = new HashMap<>();
+    romanSymbolsMap.put(1, "I");
+    romanSymbolsMap.put(2, "II");
+    romanSymbolsMap.put(3, "III");
+    romanSymbolsMap.put(4, "IV");
+    romanSymbolsMap.put(5, "V");
+    romanSymbolsMap.put(9, "IX");
+    romanSymbolsMap.put(10, "X");
+    romanSymbolsMap.put(40, "XL");
+    romanSymbolsMap.put(50, "L");
+    romanSymbolsMap.put(90, "XC");
+    romanSymbolsMap.put(100, "C");
+    romanSymbolsMap.put(400, "CD");
+    romanSymbolsMap.put(500, "D");
+    romanSymbolsMap.put(900, "CM");
+    romanSymbolsMap.put(1000, "M");
   }
 
 
   public String convert(int arabicNumber)
   {
+    StringBuilder romanNumeralSb = new StringBuilder();
     int currentArabicNumber = arabicNumber;
     do
     {
@@ -49,8 +47,8 @@ public class RomanNumerals
 
   private Map.Entry<Integer, String> findMaxSymbolEntry(int arabicNumber)
   {
-    return romanSymbolMap.entrySet().stream()
-            .filter(integerStringEntry -> arabicNumber - integerStringEntry.getKey() >= 0)
+    return romanSymbolsMap.entrySet().stream()
+            .filter(romanSymbolEntry -> arabicNumber - romanSymbolEntry.getKey() >= 0)
             .max((o1, o2) -> o1.getKey().compareTo(o2.getKey()))
             .get();
   }
